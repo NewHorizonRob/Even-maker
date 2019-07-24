96afe6432c52ae8109d74b27dc4761234065748e
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-24 17:03:11 -0400

    Update EvenMaker.cs

--- EvenMaker.cs
+++ EvenMaker.cs
@@ -13,25 +13,25 @@ namespace Ch8_EvenMaker
 
         public EvenMaker(int size) #method with an integer as a parameter 
         {
-            numbers = new int[size];
+            numbers = new int[size]; #makes the array a certain size based on the user input
         }
 
-        public void Fill() 
+        public void Fill() #method for filling the array with numbers 
         {
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < numbers.Length; i++) #goes through the numbers array  
             {
-                numbers[i] = rand.Next(1, 50);
+                numbers[i] = rand.Next(1, 50); #adds random numbers to the array between 1 and 50
             }
         }
 
-        public void MakeEven()
+        public void MakeEven() #method name to make the odd number even within the array
         {
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < numbers.Length; i++) #goes through the number array
             {
-                int number = numbers[i];
+                int number = numbers[i]; #singles out each number within the array
                 if (number % 2 != 0)
                 {
-                  numbers [i]= number + 1;
+                  numbers [i]= number + 1; #if the number in the array is odd then add one to make even 
                 }
 
             }
@@ -39,12 +39,12 @@ namespace Ch8_EvenMaker
         }
 
 
-        public void Show()
+        public void Show() #method to show the array before or after making the numbers even 
         {
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)#goes through the number array
             {
-                int number = numbers[i];
-                Console.Write($"{number} ");
+                int number = numbers[i]; #singles out each number within the array
+                Console.Write($"{number} "); #prints out each number within the array 
             }
         }
     }
