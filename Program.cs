65ea9b75fec6beb45b972ce6da6877772a47cbd0
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-24 02:24:08 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +12,12 @@ namespace Ch8_EvenMaker
         {
             Console.WriteLine("How many elements?");
             Console.Write("Enter number: ");
-            int num = Convert.ToInt32(Console.ReadLine());
+            int num = Convert.ToInt32(Console.ReadLine()); #converts the num variable to Int32
 
 
-            EvenMaker e = new EvenMaker(num);
-
+            EvenMaker e = new EvenMaker(num); #creates a new instance to call on methods from the program page
+            
+            # method calls to run the methods from the program page using the new instance
             e.Fill();
             e.Show();
             e.MakeEven();
